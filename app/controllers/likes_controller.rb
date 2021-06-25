a194a5534738ef04707f26dbec797d89843eac32
damari <motokin128@gmail.com>
2021-06-25 17:52:18 +0900

    詳細ページにいいね機能を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    current_user.likes.create!(post_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    like = current_user.likes.find_by(post_id: params[:post_id])
+    like.destroy!
+    redirect_back(fallback_location: root_path)
+  end
 end
