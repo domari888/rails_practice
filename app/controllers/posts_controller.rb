f797f62d5f762b8890481c0185eae38629be28de
damari <motokin128@gmail.com>
2021-06-25 14:07:59 +0900

    before_action の処理を追加
    ログインユーザーの投稿か確認するため

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i(edit update destroy)
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
   end
@@ -17,22 +18,23 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to post
+    @post.update!(post_params)
+    redirect_to @post
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy!
+    @post.destroy!
     redirect_to root_path
   end
 
   private
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
