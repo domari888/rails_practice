b56c437bc62c3728c33b75cf91b9de639f8152fe
damari <motokin128@gmail.com>
2021-06-25 16:55:23 +0900

    いいねを表示する処理を設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+
+  def liked_by?(current_user)
+    likes.exists?(user_id: current_user.id)
+  end
 end
