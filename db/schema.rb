72b0d371adf6b60930c1485167fde7352ea5b3f5
damari <motokin128@gmail.com>
2021-06-25 09:41:45 +0900

    Post モデルを作成して、バリデーションを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_144628) do
+ActiveRecord::Schema.define(version: 2021_06_25_003218) do
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_06_24_144628) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
