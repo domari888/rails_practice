f0e6b2f70661c6f7dffd3f2934548907bc1e41ac
damari <motokin128@gmail.com>
2021-06-25 16:39:10 +0900

    Post モデルと User モデルに追記
    中間テーブル経由で関連付けする為

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :user_name, presence: true
